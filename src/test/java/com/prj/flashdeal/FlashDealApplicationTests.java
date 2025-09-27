1c86555d2677a817ebf8353c9dca2590522765df
dogyun00 <kimdk0903@naver.com>
2025-09-28 00:20:43 +0900

    first commit

---
+++ src/test/java/com/prj/flashdeal/FlashDealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prj.flashdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashDealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
