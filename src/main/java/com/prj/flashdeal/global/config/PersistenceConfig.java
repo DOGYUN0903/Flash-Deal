80886c5877bf883900044a91d273e76bd0b0b1f9
dogyun00 <kimdk0903@naver.com>
2026-02-15 23:43:17 +0900

    refactor: PersistenceConfig 파일 삭제

--- src/main/java/com/prj/flashdeal/global/config/PersistenceConfig.java
+++
@@ -1,9 +0,0 @@
-package com.prj.flashdeal.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class PersistenceConfig {
-}
