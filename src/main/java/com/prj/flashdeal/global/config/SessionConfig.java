8d01f9dc1c311c75b46dd0a4fbae71df55c48c95
dogyun00 <kimdk0903@naver.com>
2026-03-12 23:38:01 +0900

    fix : Redis 세션 초기화 설정 추가

--- src/main/java/com/prj/flashdeal/global/config/SessionConfig.java
+++ src/main/java/com/prj/flashdeal/global/config/SessionConfig.java
@@ -5,12 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.session.FindByIndexNameSessionRepository;
 import org.springframework.session.Session;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisIndexedHttpSession;
 import org.springframework.session.security.SpringSessionBackedSessionRegistry;
 
 @Configuration
 @EnableRedisIndexedHttpSession
 public class SessionConfig {
+
+    @Bean
+    public ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
 
     @Bean
     public SessionRegistry sessionRegistry(
